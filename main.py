71227772eabe94373cabe63886ba13881b4c6b4c
fuhrm.max <fuhrmann.maxime@hotmail.fr>
2025-05-01 12:17:48 +0200

    B: add final touch to bookbot

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 
+import sys
+['main.py', 'books/frankenstein.txt']
+['main.py', 'books/ mobydick.txt']
+['main.py', 'books/prideandprejudice.txt']
+
 def get_book_text(f):
     with open(f) as b:
         return b.read()
     
 def main():
-    result = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_file = sys.argv[1]
+    result = get_book_text(sys.argv[1])
     new_result = char_count(result)
     words = words_counte(result)
     sorted_result = char_organizer(new_result)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found in {book_file}")
     print("----------- Word Count ----------")
     print(f"Found {words} total words")
     print("--------- Character Count -------")
---
+++ sysdoc.py
@@ -0,0 +1,2 @@
+import sys
+sys.path.append("/home/fuhrm/boot.dev.ls/github.com/maximeF97/bookbot/main.py") 
